182cdc2db3c011aacb78eb9ff274395ea4163e02
kuliesh <s.kuliesh>
2019-10-09 17:54:24 +0300

    1. update code

--- src/test/java/net/ukr/mailauto/Tests/listeners/EventsListener.java
+++ src/test/java/net/ukr/mailauto/Tests/listeners/EventsListener.java
@@ -7,18 +7,22 @@ import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 
 public class EventsListener extends AbstractWebDriverEventListener {
 
+    @Override
     public void beforeFindBy(By by, WebElement element, WebDriver driver) {
         System.out.println(("[Розпочато пошук локатора:] " + by));
     }
 
+    @Override
     public void afterFindBy(By by, WebElement element, WebDriver driver) {
         System.out.println(("[Локатор"+ by +"]  було знайдено"));
     }
 
+    @Override
     public void beforeClickOn(WebElement element, WebDriver driver) {
         System.out.println(("[Виконано клік по елементу]" + element));
     }
 
+    @Override
     public void onException(Throwable throwable, WebDriver driver) {
         // Do nothing
     }
